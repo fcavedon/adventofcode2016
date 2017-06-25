ce9e201f56157eba10be05d13182b999aafb2622
Francisco Cavedon <francisco.cavedon@shopify.com>
2017-06-25 11:40:12 -0400

    fix output visualization

--- app/src/main/kotlin/com/shopify/adventofcode/day8/Main.kt
+++ app/src/main/kotlin/com/shopify/adventofcode/day8/Main.kt
@@ -1,35 +1,42 @@
 package com.shopify.adventofcode.day8
 
+import com.shopify.adventofcode.Point
+import com.shopify.adventofcode.asInt
+
 /**
  * Created by cavedon on 2017-06-01.
  */
 fun main(args: Array<String>) {
 
-    val screen = Screen()
+    val screen = Screen(5, 49)
 
     for (line in input) {
         if (line.contains("rect")) {
             val cmd = line.split("rect", "x")
-            screen.rect(cmd[1].trim().toInt(), cmd[2].trim().toInt())
+            screen.rect(cmd[1].asInt(), cmd[2].asInt())
         }
 
-        if (line.contains("rotate row")) {
+        if (line.contains("row")) {
             val cmd = line.split("y=", "by")
-            screen.rotate(Component.ROW, cmd[1].trim().toInt(), cmd[2].trim().toInt())
+            screen.rotate(Component.ROW, cmd[1].asInt(), cmd[2].asInt())
         }
-        if (line.contains("rotate column")) {
+        if (line.contains("column")) {
             val cmd = line.split("x=", "by")
-            screen.rotate(Component.COLUMN, cmd[1].trim().toInt(), cmd[2].trim().toInt())
+            screen.rotate(Component.COLUMN, cmd[1].asInt(), cmd[2].asInt())
 
         }
     }
     screen.print()
-    println()
-    println(screen.pixels.count { it.on })
-
+    println(screen.pixels.filter { it.on }.count())
 }
 
-data class Pixel(var x: Int, var y: Int, var on: Boolean = false)
+data class Pixel(override var x: Int, override var y: Int, var on: Boolean = false) : Point(x, y) {
+    override fun toString(): String {
+        if (on) return "#"
+
+        return " "
+    }
+}
 
 class Screen(val rows: Int = 2, val columns: Int = 6) {
     var pixels = ArrayList<Pixel>()
@@ -37,37 +44,26 @@ class Screen(val rows: Int = 2, val columns: Int = 6) {
     init {
         for (row in 0..rows) {
             for (column in 0..columns) {
-                pixels.add(Pixel(row, column))
+                pixels.add(Pixel(column, row))
             }
         }
     }
 
     fun rect(x: Int, y: Int) {
-        pixels.filter {
-            it.y in 0 until x && it.x in 0 until y
-
-        }.map { it.on = true }
+        pixels.filter { it.x < x && it.y < y }.map { it.on = true }
     }
 
     fun rotate(component: Component, target: Int, amount: Int) {
         when (component) {
             Component.COLUMN -> {
-                pixels.filter { it.y == target }.map { p ->
-                    if (p.x + amount > rows) {
-                        p.x = ((p.x + amount) % rows) - 1
-                    } else {
-                        p.x += amount
-                    }
-                }
-
-            }
-            Component.ROW -> {
                 pixels.filter { it.x == target }.map { p ->
-                    if (p.y + amount > columns) {
-                        p.y = ((p.y + amount) % columns) - 1
-                    } else {
-                        p.y += amount
-                    }
+                    p.y = (p.y + amount) % (rows + 1)
+                }
+            }
+
+            Component.ROW -> {
+                pixels.filter { it.y == target }.map { p ->
+                    p.x = (p.x + amount) % (columns + 1)
                 }
             }
         }
@@ -75,17 +71,14 @@ class Screen(val rows: Int = 2, val columns: Int = 6) {
 
     fun print() {
         for (i in 0..rows) {
-            val row = pixels.filter { it.x == i }
-            row.forEach {
-                if (it.on) {
-                    print("#")
-                } else {
-                    print(".")
-                }
+            pixels.filter { it.y == i }.sortedBy { it.x }.forEachIndexed { index, pixel ->
+                print(pixel)
+                if (index + 1 % 5 == 0) print("  ")
             }
             println()
         }
     }
+
 }
 
 enum class Component {
