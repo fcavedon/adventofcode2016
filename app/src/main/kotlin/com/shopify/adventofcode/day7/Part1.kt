46219f00e2be8512cf8dd3e887905c9cbe1e28d9
Francisco Cavedon <francisco.cavedon@shopify.com>
2017-06-26 10:28:07 -0400

    cleaning up code (still gives off-by-one answer!!)

---
+++ app/src/main/kotlin/com/shopify/adventofcode/day7/Part1.kt
@@ -0,0 +1,31 @@
+package com.shopify.adventofcode.day7
+
+import com.shopify.adventofcode.isEven
+
+/**
+ * Created by cavedon on 2017-06-25.
+ */
+fun main(args: Array<String>) {
+
+    println(input.count { line ->
+        val split = line.split("[", "]")
+
+        val (supernet, hypernet) = split.map { Pair(split.indexOf(it), it) }.partition { it.first.isEven() }
+
+        val validSupernet = supernet.map { isAbba(it.second) }.any { it }
+        val validHypernet = hypernet.map { !isAbba(it.second) }.any { it }
+
+        validHypernet && validSupernet
+    })
+
+}
+
+fun isAbba(input: String): Boolean {
+    with(input) {
+        return (4 until length).withIndex()
+                .map { substring(it.index, it.value).isPalindrome() }
+                .any { it }
+    }
+}
+
+fun String.isPalindrome(): Boolean = this[0] == this[3] && this[1] == this[2] && this[0] != this[1]
